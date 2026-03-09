3c630b976953f610b629d87d006d391ed21a0f91
YaroslavPanasiuk <yaroslav.panasiuk@lnu.edu.ua>
2026-03-09 23:12:19 +0200

    init

--- bot/utils/schedulers.py
+++ bot/utils/schedulers.py
@@ -1,10 +1,6 @@
 import asyncio
 from aiogram import Bot
-from aiogram.exceptions import TelegramForbiddenError, TelegramRetryAfter
-from bot.db import database
-from bot.lexicon import select_random_line
 from bot.utils.keyboards import get_main_menu_keyboard
-from bot.utils.spreadsheets import fetch_users_with_no_stats
 from datetime import datetime
 from bot.config import Config
 from bot.lexicon import Lexicon
